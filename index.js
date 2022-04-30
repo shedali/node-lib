92f7e94c7160e9a35398c24229ea8da1e8858816
Franz Sittampalam <shedali@gmail.com>
2022-04-30 23:03:10 +0100

    update template for vite types

--- index.js
+++
@@ -1,3 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-fs.readFileSync(path.join(process.cwd(), ));//process.argv[0]
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("hello");
+export const doit = () => {};
+/// <reference types="vite/types/importMeta" />
+
+// in-source test suites
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest;
+  it("add", () => {});
+}
